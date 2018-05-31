eb50276f21705d0a89fe29bbfe83cb167b53004b
con2000us <s0321039@gmail.com>
2018-05-31 17:10:23 +0800

    0531_tesseractOCR_test

--- src/mainbot/Auto.java
+++ src/mainbot/Auto.java
@@ -3,6 +3,7 @@ package mainbot;
 import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
@@ -11,13 +12,17 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import MyRobot.MyRobot;
+import net.sourceforge.tess4j.*;
 
 public class Auto {
 	
 
 	public static void main(String[] args) throws AWTException {
 		// TODO Auto-generated method stub
+		File imageFile = new File("eurotext.png");
 		
+		ITesseract instance = new Tesseract();  // JNA Interface Mapping
+		instance.setLanguage("eng");
 		Robot robot = new Robot();
 		Color color;
 		
@@ -32,13 +37,33 @@ public class Auto {
 		    byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
 		    System.out.println(pixels.length);
 		    
-		    for(int i=0;i<100;i+=5) {
-		    	color = robot.getPixelColor(0, i);
-		    	robot.mouseMove(0,i);
-		    	System.out.print((color.getRed()+color.getGreen()+color.getBlue())/3+ " ");
-		    }
+
+//		    for(int i=185;i<=197;i++) {
+//		    	for(int j=2720;j<=2760;j++){
+//			    	color = robot.getPixelColor(j, i);
+//			    	if(color.getRed()+color.getGreen()+color.getBlue() > 200){
+//				    	System.out.print(" ");
+//			    	}else {
+//				    	System.out.print("@");
+//			    	}
+//
+//		    	}
+//		    	System.out.println("|");
+//	
+//		    }
+	    	robot.mouseMove(2720,185);
+	    	robot.mouseMove(2760,197);
+	    	robot.mousePress(InputEvent.BUTTON1_MASK);
+	        robot.mouseRelease(InputEvent.BUTTON1_MASK);
 		} catch (IOException e) {
 		}
+		
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
 	}
 
 }
